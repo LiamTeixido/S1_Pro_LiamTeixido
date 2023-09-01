4e88256f70d982d7af2027df979ca8fe14aba8d3
LiamTeixido <liamteixido@gmail.com>
2023-08-31 21:36:26 -0500

    If-Else_Switch - LiamTeixido

---
+++ Assets/Scripts/E1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E1 : MonoBehaviour
+{
+    void Start()
+    {
+        if (1 >= 1)
+        {
+            Debug.Log("POSITIVO");
+        }
+        else 
+        {
+            Debug.Log("NEGATIVO");
+        }
+    }
+}
---
+++ Assets/Scripts/E2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E2 : MonoBehaviour
+{
+    void Start()
+    {
+        int order;
+        order = 4;
+        switch (order)
+        {
+            case 1:
+                Debug.Log("Pera");
+                break;
+            case 2:
+                Debug.Log("Fresa");
+                break;
+            case 3:
+                Debug.Log("Papaya");
+                break;
+            case 4:
+                Debug.Log("Mandarina");
+                break;
+        }
+    }
+    }
